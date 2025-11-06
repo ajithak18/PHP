8473c7e12243e668b34646ba8ddd2f2842ef3a5a
ajithak18 <ajithak1815@gmail.com>
2025-11-06 20:42:20 +0530

    Add files via upload

---
+++ css.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Php program</title>
+</head>
+<body>
+<h1 style="color: blue; text-align: center;">Sales Dashboard</h1>
+<h2 style="color: green;">Tax.</h2>
+<?php
+$name = "Money";
+echo "<h3>I love $name</h3><br>"; 
+$x=5;
+var_dump($x);
+?>
+</body>
+</html>
